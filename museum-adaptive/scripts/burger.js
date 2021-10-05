cc23548adc415e810d3a795ae5ce95963de76ad0
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2021-10-06 01:33:03 +0300

    feat: enable console

--- museum-adaptive/scripts/burger.js
+++ museum-adaptive/scripts/burger.js
@@ -1,16 +1,21 @@
 const adaptiveMenu = document.querySelector('.welcome > aside');
 const burgerMenu = document.querySelector('.burger-menu');
 const closeBurger = document.querySelector('.adaptive-menu > .close');
+const textBehind = document.querySelector('.welcome .text');
+
+console.log(textBehind);
 
 function adaptiveMenuHandler(ev) {
   if (this === burgerMenu) {
     burgerMenu.style.display = 'none';
     closeBurger.style.display = 'inline-block';
     adaptiveMenu.style.marginLeft = '0';
+    textBehind.style.display = 'none';
   } else {
     burgerMenu.style.display = 'inline-block';
     closeBurger.style.display = 'none';
     adaptiveMenu.style.marginLeft = '-100%';
+    textBehind.style.display = 'block';
   }
 }
 

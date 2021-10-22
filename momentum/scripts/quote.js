50227df1ae77aef6cbab3832900387b7f7f0ebc4
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2021-10-22 20:14:32 +0300

    feat: add quote feature 2021-10-22 20:14:31

---
+++ momentum/scripts/quote.js
@@ -0,0 +1,22 @@
+const quoteRandomizer = document.querySelector('.quote-new');
+const quoteText = document.querySelector('.quote-text');
+const quoteAuthor = document.querySelector('.quote-author');
+
+console.log({ quoteText, quoteAuthor });
+
+async function getQuotes() {
+  const randomQuoteNumber = Math.floor(Math.random() * 99);
+  const quoteFile = '../assets/quotes.eng.json';
+  const response = await fetch(quoteFile);
+  const data = await response.json();
+  quoteText.textContent = data[randomQuoteNumber].text;
+  quoteAuthor.textContent = data[randomQuoteNumber].author;
+}
+
+getQuotes();
+
+function quoteHandler() {
+  getQuotes();
+}
+
+quoteRandomizer.addEventListener('click', quoteHandler);

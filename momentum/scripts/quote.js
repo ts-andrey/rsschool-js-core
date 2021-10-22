2ab852b83b7c1ccfe4f8372658b9e34e2044859b
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2021-10-22 20:27:37 +0300

    fix: attemp to fix links 2021-10-22 20:27:37

--- momentum/scripts/quote.js
+++ momentum/scripts/quote.js
@@ -6,7 +6,7 @@ console.log({ quoteText, quoteAuthor });
 
 async function getQuotes() {
   const randomQuoteNumber = Math.floor(Math.random() * 99);
-  const quoteFile = '../assets/quotes.eng.json';
+  const quoteFile = './assets/quotes.eng.json';
   const response = await fetch(quoteFile);
   const data = await response.json();
   quoteText.textContent = data[randomQuoteNumber].text;

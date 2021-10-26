d8226b12e89992dc9cb70ef39076c018eff1d0b7
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2021-10-26 22:13:06 +0300

    feat: add todo save and delete completed on reload 2021-10-26 (22:13:06)

--- momentum/scripts/toDo.js
+++ momentum/scripts/toDo.js
@@ -6,41 +6,73 @@ const addNewToDo = document.querySelector('.add-new');
 
 let checkers = document.querySelectorAll('.checker');
 
+let toDoArr;
+if (window.localStorage.getItem('toDoList')) {
+  toDoArr = JSON.parse(window.localStorage.getItem('toDoList'));
+} else toDoArr = [];
+
+let currTime;
+let lastCurrTime;
+
+function addDeed(value) {
+  let check = true;
+  lastCurrTime = currTime;
+  currTime = new Date().getTime();
+  while (check) {
+    currTime = new Date().getTime();
+    if (lastCurrTime !== currTime) check = false;
+  }
+  const newDeed = document.createElement('li');
+  newDeed.classList.add('to-dos-item');
+
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  checkBox.classList.add('checker');
+  checkBox.id = `item-${currTime}`;
+
+  const label = document.createElement('label');
+  label.htmlFor = `item-${currTime}`;
+  label.classList.add(`item-${currTime}`);
+  label.textContent = value;
+
+  newDeed.append(checkBox);
+  newDeed.append(label);
+
+  toDoList.append(newDeed);
+  checkers = document.querySelectorAll('.checker');
+
+  checkers.forEach(el => {
+    el.removeEventListener('change', checkerHandler);
+    el.addEventListener('change', checkerHandler);
+  });
+}
+
 function checkerHandler() {
   const id = this.id;
   const el = document.querySelector(`.${id}`);
   el.classList.toggle('crossed');
+  if (el.classList.contains('crossed')) toDoArr = toDoArr.filter(element => element !== el.textContent);
+  else if (!el.classList.contains('crossed')) toDoArr.push(el.textContent);
+  window.localStorage.setItem('toDoList', JSON.stringify(toDoArr));
 }
 
 function toDoHandler() {
   if (newToDo.value) {
+    toDoArr.push(newToDo.value);
     const value = newToDo.value;
     newToDo.value = '';
-    const date = new Date();
-    const newDeed = document.createElement('li');
-    newDeed.classList.add('to-dos-item');
+    addDeed(value);
+    window.localStorage.setItem('toDoList', JSON.stringify(toDoArr));
+  }
+}
 
-    const checkBox = document.createElement('input');
-    checkBox.type = 'checkbox';
-    checkBox.classList.add('checker');
-    checkBox.id = `item-${date.getTime()}`;
-
-    const label = document.createElement('label');
-    label.htmlFor = `item-${date.getTime()}`;
-    label.classList.add(`item-${date.getTime()}`);
-    label.textContent = value;
-
-    newDeed.append(checkBox);
-    newDeed.append(label);
-
-    toDoList.append(newDeed);
-    checkers = document.querySelectorAll('.checker');
-
-    checkers.forEach(el => {
-      el.removeEventListener('change', checkerHandler);
-      el.addEventListener('change', checkerHandler);
+function setToDos() {
+  if (toDoArr.length > 0) {
+    toDoArr.forEach(el => {
+      addDeed(el);
     });
   }
 }
 
 addNewToDo.addEventListener('click', toDoHandler);
+window.addEventListener('load', setToDos);

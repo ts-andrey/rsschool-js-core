87bf58809f0e3336f7d30fda62cc2f730d93746e
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2021-10-26 03:09:07 +0300

    feat: add styles for lower resolutions 2021-10-26 (03:09:06)

--- momentum/scripts/toDo.js
+++ momentum/scripts/toDo.js
@@ -1,6 +1,7 @@
 const toDoList = document.querySelector('.to-dos');
 
-const newToDo = document.querySelector('.to-do');
+const newToDo = document.querySelector('.to-do-new-item');
+// const newToDoList = document.querySelector('.to-do-new-list');
 const addNewToDo = document.querySelector('.add-new');
 
 let checkers = document.querySelectorAll('.checker');
@@ -12,33 +13,34 @@ function checkerHandler() {
 }
 
 function toDoHandler() {
-  const value = newToDo.value;
-  newToDo.value = '';
-  const date = new Date();
-  const newDeed = document.createElement('li');
-  newDeed.classList.add('to-dos-item');
+  if (newToDo.value) {
+    const value = newToDo.value;
+    newToDo.value = '';
+    const date = new Date();
+    const newDeed = document.createElement('li');
+    newDeed.classList.add('to-dos-item');
 
-  const checkBox = document.createElement('input');
-  checkBox.type = 'checkbox';
-  checkBox.classList.add('checker');
-  checkBox.id = `item-${date.getTime()}`;
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    checkBox.classList.add('checker');
+    checkBox.id = `item-${date.getTime()}`;
 
-  const label = document.createElement('label');
-  label.htmlFor = `item-${date.getTime()}`;
-  label.classList.add(`item-${date.getTime()}`);
-  label.textContent = value;
+    const label = document.createElement('label');
+    label.htmlFor = `item-${date.getTime()}`;
+    label.classList.add(`item-${date.getTime()}`);
+    label.textContent = value;
 
-  newDeed.append(checkBox);
-  newDeed.append(label);
+    newDeed.append(checkBox);
+    newDeed.append(label);
 
-  toDoList.append(newDeed);
-  checkers = document.querySelectorAll('.checker');
+    toDoList.append(newDeed);
+    checkers = document.querySelectorAll('.checker');
 
-  checkers.forEach(el => {
-    el.removeEventListener('change', checkerHandler);
-    el.addEventListener('change', checkerHandler);
-  });
-  
+    checkers.forEach(el => {
+      el.removeEventListener('change', checkerHandler);
+      el.addEventListener('change', checkerHandler);
+    });
+  }
 }
 
 addNewToDo.addEventListener('click', toDoHandler);
